7f5a33165c353c5a1657ad968313bbd2d2ecf038
Joe McCain III <jo3mccain@icloud.com>
2025-02-01 14:31:11 -0600

    update shift-form.tsx
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- src/features/shifts/widgets/shift-form.tsx
+++ src/features/shifts/widgets/shift-form.tsx
@@ -137,6 +137,7 @@ export const TimesheetForm: React.FC<
               <FormLabel>Date</FormLabel>
               <FormControl>
                 <DatePickerPopover
+                  mode="single"
                   onDateSelect={(date: any) => field.onChange(new Date(date))}
                   selected={field.value}
                 />
