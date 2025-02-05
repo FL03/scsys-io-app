27f3e764e8c2412538a6d326b7dce635d5528c1b
Joe McCain III <jo3mccain@icloud.com>
2025-02-05 09:52:23 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- src/features/profiles/provider.tsx
+++ src/features/profiles/provider.tsx
@@ -8,13 +8,14 @@ import * as React from 'react';
 import { Profile, fetchUserProfile } from '@/features/profiles';
 import { createBrowserClient } from '@/utils/supabase';
 import { Nullish } from '@/types';
+import { REALTIME_SUBSCRIBE_STATES, RealtimeChannel, RealtimePostgresChangesPayload } from '@supabase/supabase-js';
 
 type ProfileContext = {
   profile?: Profile | null;
   setProfile: React.Dispatch<React.SetStateAction<Nullish<Profile>>>;
   uid?: string | null;
   username?: string | null;
-};
+} & { [key: string]: any };
 
 export const ProfileContext = React.createContext<ProfileContext>({
   profile: null,
@@ -40,6 +41,8 @@ export const ProfileProvider = React.forwardRef<
   const supabase = createBrowserClient();
   // initialize the profile state
   const [_profile, _setProfile] = React.useState<Nullish<Profile>>(null);
+
+  const channelRef = React.useRef<Nullish<RealtimeChannel>>(null);
   // create a callback for loading the profile data
   const loader = React.useCallback(
     async (alias?: string | null) => {
@@ -47,37 +50,44 @@ export const ProfileProvider = React.forwardRef<
         const data = await fetchUserProfile({ username: alias });
         if (data) _setProfile(data);
       }
-      
     },
     [_setProfile]
   );
+  const onChange = React.useCallback((payload: RealtimePostgresChangesPayload<Profile>) => {
+    const data = payload.new as Profile;
+
+    if (payload.eventType === 'INSERT' || payload.eventType === 'UPDATE') {
+      _setProfile(payload.new);
+    }
+  }, [_setProfile]);
+  const onSubscribe = React.useCallback(
+        (callback: (status: REALTIME_SUBSCRIBE_STATES, err?: Error) => void, timeout?: number) => {
+          channelRef.current?.subscribe(callback, timeout);
+        },
+        [channelRef]
+      );
   // subscribe to profile changes
   React.useEffect(() => {
     // if null, load the profile data
     if (!_profile) loader(username);
-    const channel = supabase
-      .channel(`profiles:${_profile?.username ?? username}`)
-      .on(
-        'postgres_changes',
-        {
-          event: '*',
-          schema: 'public',
-          table: 'profiles',
-        },
-        (payload) => {
-          if (payload.new) _setProfile(payload.new as any);
-        }
-      )
-      .subscribe((status, err) => {
-        if (err) throw err;
-        if (status === 'SUBSCRIBED') {
-          console.log('Profile changes subscribed');
-        }
-      });
+  }, [_profile, loader, username]);
+
+  React.useEffect(() => {
+    if (username) {
+      channelRef.current = supabase
+        .channel(`profiles:${username}`, { config: { private: true } })
+        .on(
+          'postgres_changes',
+          { event: '*', schema: 'public', table: 'profiles' },
+          onChange
+        );
+    }
     return () => {
-      channel.unsubscribe();
+      if (channelRef.current) {
+        supabase?.realtime.removeChannel(channelRef.current);
+      }
     };
-  }, [_setProfile, _profile, loader, supabase, username]);
+  }, [channelRef, supabase, username, _setProfile]);
   // get the profile state
   const profile = _profile;
   // create a setter function
@@ -90,8 +100,9 @@ export const ProfileProvider = React.forwardRef<
       username: profile?.username,
       loader,
       setProfile,
+      onSubscribe,
     }),
-    [profile, loader, setProfile]
+    [profile, loader, onSubscribe, setProfile]
   );
   return (
     <ProfileContext.Provider value={ctx}>
