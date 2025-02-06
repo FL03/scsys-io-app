fb51d6fc87632b3c7bc357f17b874e04c07896fc
Joe McCain III <jo3mccain@icloud.com>
2025-02-06 08:45:09 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- src/features/shifts/provider.tsx
+++ src/features/shifts/provider.tsx
@@ -8,6 +8,7 @@ import * as React from 'react';
 import {
   REALTIME_SUBSCRIBE_STATES,
   RealtimeChannel,
+  RealtimePostgresChangesPayload,
 } from '@supabase/supabase-js';
 
 // project
@@ -17,6 +18,22 @@ import { createBrowserClient } from '@/utils/supabase';
 // feature-specific
 import { Timesheet } from './types';
 import { adjustedDate, fetchUsersTips } from './utils';
+
+const handleNewData = (value: Timesheet) => {
+  return { ...value, date: adjustedDate(value.date).toISOString() };
+};
+
+const handleSubscribe = (status: REALTIME_SUBSCRIBE_STATES, err?: Error) => {
+  if (status === 'SUBSCRIBED') {
+    logger.info('Subscribed to shifts channel');
+  }
+  if (status === 'CLOSED') {
+    logger.info('Closed the shifts channel');
+  }
+  if (err) {
+    logger.error('Error subscribing to shifts channel', err);
+  }
+};
 
 type ScheduleContext = {
   shifts?: Timesheet[] | null;
@@ -39,15 +56,16 @@ export const useSchedule = () => {
 export const ScheduleProvider: React.FC<
   React.PropsWithChildren<{ username: string }>
 > = ({ children, username }) => {
-    // initialize the supabase client
-    const supabase = createBrowserClient();
-    // declared a state to check if the hook has been initialized
-    const [_initialized, _setInitialized] = React.useState<boolean>(false);
-    // initialize the shifts state
-    const [_shifts, _setShifts] = React.useState<Nullish<Timesheet[]>>(null);
-    // create a loader callback
-    const loader = React.useCallback(
-      async (alias?: string) => {
+  // initialize the supabase client
+  const supabase = createBrowserClient();
+  // declared a state to check if the hook has been initialized
+  const [_initialized, _setInitialized] = React.useState<boolean>(false);
+  // initialize the shifts state
+  const [_shifts, _setShifts] = React.useState<Nullish<Timesheet[]>>(null);
+  // create a loader callback
+  const _loadShifts = React.useCallback(
+    async (alias?: string) => {
+      try {
         let data = await fetchUsersTips(alias);
         data = data?.map(({ date, ...shift }) => {
           return {
@@ -56,103 +74,85 @@ export const ScheduleProvider: React.FC<
           };
         });
         if (data) _setShifts(data);
-      },
-      [fetchUsersTips, _setShifts]
-    );
-    // create a channel reference
-    const channelRef = React.useRef<Nullish<RealtimeChannel>>(
-      supabase.channel(`shifts:${username}`, { config: { private: true } })
-    );
-    // create a callback to handle postgres_changes
-    const handleChanges = React.useCallback(
-      (payload:{ [key: string]: any }) => {
-        logger.info('Change detected within the shifts table');
-        const newData = payload.new as Timesheet;
-        if (payload.eventType === 'INSERT') {
-          logger.info('New shift detected');
-          _setShifts((v) => {
-            return v ? [...v, newData] : [newData];
-          });
-        }
-        if (payload.eventType === 'UPDATE') {
-          logger.info('Shift updated');
-          _setShifts((v) => {
-            return v?.map((shift) => {
-              return shift.id === newData.id ? newData : shift;
-            });
-          });
-        }
-        if (payload.eventType === 'DELETE') {
-          logger.info('Shift deleted');
-          _setShifts((v) => {
-            return v?.filter((shift) => shift.id !== newData.id);
-          });
-        }
-      },
-      [_setShifts]
-    );
+      } catch (err) {
+        logger.error('Error loading shifts', err);
+      } finally {
+        _setInitialized(true);
+      }
+    },
+    [_setShifts, _setInitialized]
+  );
+  // create a channel reference
+  const _channel = React.useRef<Nullish<RealtimeChannel>>(
+    supabase.channel(`shifts:${username}`, { config: { private: true } })
+  );
+  // create a callback to handle postgres_changes
+  const _onChange = React.useCallback(
+    (payload: RealtimePostgresChangesPayload<Timesheet>) => {
+      logger.info('Processing changes to the shifts table');
+      const data = handleNewData(payload.new as Timesheet);
+      if (payload.eventType === 'INSERT') {
+        logger.info('New shift detected');
 
-    const handleSubscribe = React.useCallback((status: REALTIME_SUBSCRIBE_STATES, err?: Error) => {
-      if (status === 'SUBSCRIBED') {
-        logger.info('Subscribed to shifts channel');
+        _setShifts((v) => {
+          return v ? [...v, data] : [data];
+        });
       }
-      if (status === 'CLOSED') {
-        logger.info('Closed the shifts channel');
+      if (payload.eventType === 'UPDATE') {
+        logger.info('Shift updated');
+        _setShifts((v) => {
+          return v?.map((shift) => {
+            return shift.id === data.id ? data : shift;
+          });
+        });
       }
-      if (err) {
-        logger.error('Error subscribing to shifts channel', err);
+      if (payload.eventType === 'DELETE') {
+        logger.info('Shift deleted');
+        _setShifts((v) => {
+          return v?.filter((shift) => shift.id !== data.id);
+        });
       }
-    }, []);
-  
-    const onSubscribe = React.useCallback(
-      (callback: (status: REALTIME_SUBSCRIBE_STATES, err?: Error) => void, timeout?: number) => {
-        channelRef.current?.subscribe(callback, timeout);
-      },
-      [channelRef]
-    );
-    // load the shifts
-    React.useEffect(() => {
-      // if null, load the shifts data
-      if (!_shifts) {
-        loader(username);
+    },
+    [_setShifts]
+  );
+
+  // load the shifts
+  React.useEffect(() => {
+    // if null, load the shifts data
+    if (!_initialized && !_shifts) _loadShifts(username);
+  }, [_shifts, _loadShifts, username]);
+  // realtime effects
+  React.useEffect(() => {
+    // if the channel is not initialized, initialize it
+    if (!_channel.current) {
+      _channel.current = supabase
+        .channel(`shifts:${username}`, { config: { private: true } })
+        .on(
+          'postgres_changes',
+          {
+            event: '*',
+            filter: `assignee=eq.${username}`,
+            schema: 'public',
+            table: 'shifts',
+          },
+          _onChange
+        )
+        .subscribe(handleSubscribe);
+    }
+
+    return () => {
+      if (_channel.current) {
+        supabase.removeChannel(_channel.current);
+        _channel.current = null;
       }
-    }, [_shifts, loader, username]);
-    // realtime effects
-    React.useEffect(() => {
-      // if the channel is not initialized, initialize it
-      if (!channelRef.current) {
-        channelRef.current = supabase
-          .channel(`shifts:${username}`, { config: { private: true } })
-          .on(
-            'postgres_changes',
-            {
-              event: '*',
-              filter: `assignee=eq.${username}`,
-              schema: 'public',
-              table: 'shifts',
-            },
-            handleChanges
-          ).subscribe(
-            handleSubscribe
-          );
-      }
-  
-      return () => {
-        if (channelRef.current) {
-          supabase.removeChannel(channelRef.current);
-          channelRef.current = null;
-        }
-      };
-    }, [
-      _initialized, _shifts, handleChanges, channelRef, supabase, username,
-    ]);
-    const channel = channelRef.current;
-    // redeclare the shifts
-    const shifts = _shifts;
-    // create a callback to set the shifts
-    const setShifts = React.useCallback(_setShifts, [_setShifts]);
-    // create the context
-    const ctx = React.useMemo(() => ({ shifts, onSubscribe, setShifts }), [shifts, onSubscribe, setShifts]);
+    };
+  }, [supabase, username, _channel, _onChange]);
+  // redeclare the shifts
+  const shifts = _shifts;
+  // create a callback to set the shifts
+  const setShifts = React.useCallback(_setShifts, [_setShifts]);
+  // create the context
+  const ctx = React.useMemo(() => ({ shifts, setShifts }), [shifts, setShifts]);
   // return the provider
   return <ScheduleContext value={ctx}>{children}</ScheduleContext>;
 };
